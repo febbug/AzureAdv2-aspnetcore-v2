84896bd69a89678b0f35203ceb2441063f8a4312
febbug <jmarkovec@gmail.com>
2018-02-25 19:15:16 +0000

    initial taken from various online examples

---
+++ TodoListService/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Globalization;
+
+namespace TodoListService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            //.AddAzureAdBearer(options => Configuration.Bind("AzureAd", options))
+            .AddJwtBearer(o =>
+            {
+                o.Audience = Configuration["AzureAd:ClientId"];
+                o.Authority = "https://login.microsoftonline.com/af558750-d574-4458-973b-c8ef7ee0801a/v2.0";
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
